db839a414db8430d740ccda43822d08af03b937e
zwdkoei <48703932+zwdkoei@users.noreply.github.com>
2020-08-31 11:52:50 +0800

    test91 1

---
+++ src/main/java/com/example/test91/Test91Application.java
@@ -0,0 +1,13 @@
+package com.example.test91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test91Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test91Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test91/Test91ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test91ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
